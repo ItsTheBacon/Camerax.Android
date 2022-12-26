aab92571d5ca759df5aba4ad7f3ae4df57c85a69
ItsThebacon <baconnkl@gmail.com>
2022-12-26 23:27:30 +0600

    toast extensions

---
+++ app/src/main/java/com/bacon/camerax/android/presentation/extensions/UIExtensions.kt
@@ -0,0 +1,29 @@
+package com.bacon.camerax.android.presentation.extensions
+
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+
+// region Fragment
+
+/**
+ * Fragment Extensions
+ */
+
+fun Fragment.showToastShort(text: String) {
+    Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+}
+
+fun Fragment.showToastShort(@StringRes textFromRes: Int) {
+    Toast.makeText(context, textFromRes, Toast.LENGTH_SHORT).show()
+}
+
+fun Fragment.showToastLong(text: String) {
+    Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+}
+
+fun Fragment.showToastLong(@StringRes textFromRes: Int) {
+    Toast.makeText(context, textFromRes, Toast.LENGTH_LONG).show()
+}
+
+// endregion
