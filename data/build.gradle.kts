18d48cc29dc04f95d1262a532ebc5a7054ab4330
ItsThebacon <baconnkl@gmail.com>
2022-12-26 23:56:20 +0600

    modularization - create data module

---
+++ data/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    id(Plugins.AGP.library)
+    kotlin(Plugins.Kotlin.android)
+
+    // Kotlin Symbol Processing
+    id(Plugins.KSP.ksp) version Versions.KSP
+}
+
+android {
+    namespace = "com.bacon.data"
+    compileSdk = AndroidConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AndroidConfig.minSdk
+        targetSdk = AndroidConfig.targetSdk
+    }
+
+    buildTypes {
+        getByName(AndroidConfig.release) {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+            buildConfigField("String", "BASE_URL", "\"https://example/\"")
+        }
+
+        getByName(AndroidConfig.debug) {
+            buildConfigField("String", "BASE_URL", "\"https://example.com.debug/\"")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = Options.compileOptions
+        targetCompatibility = Options.compileOptions
+    }
+    kotlinOptions {
+        jvmTarget = Options.kotlinOptions
+    }
+}
+
+dependencies {
+
+    implementation(project(":domain"))
+
+    // Retrofit
+    implementation(Libraries.Retrofit.retrofit)
+    implementation(Libraries.Retrofit.converterGson)
+
+    // OkHttp
+    implementation(Libraries.OkHttp.bom)
+    implementation(Libraries.OkHttp.okHttp)
+    implementation(Libraries.OkHttp.loggingInterceptor)
+
+    // Room
+    api(Libraries.Room.runtime)
+    ksp(Libraries.Room.compiler)
+
+    // Paging
+    api(Libraries.Paging.runtime)
+}
