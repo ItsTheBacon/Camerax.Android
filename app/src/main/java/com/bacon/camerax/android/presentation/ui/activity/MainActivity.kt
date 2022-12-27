ba4f3184caff01a9fec0e67d6e4aa577c6028b44
ItsThebacon <baconnkl@gmail.com>
2022-12-27 16:51:02 +0600

    implements navigation component in activity and create nav_graph

--- app/src/main/java/com/bacon/camerax/android/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/bacon/camerax/android/presentation/ui/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package com.bacon.camerax.android.presentation.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bacon.camerax.android.R
 import com.bacon.camerax.android.databinding.ActivityMainBinding
@@ -10,8 +12,11 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private val binding by viewBinding(ActivityMainBinding::bind)
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
