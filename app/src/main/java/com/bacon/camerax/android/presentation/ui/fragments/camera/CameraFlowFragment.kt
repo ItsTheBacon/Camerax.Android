ab986897800b0b7282b0733bfbd6ef8e094138cf
ItsThebacon <baconnkl@gmail.com>
2022-12-27 16:53:28 +0600

    CameraFlowFragment

---
+++ app/src/main/java/com/bacon/camerax/android/presentation/ui/fragments/camera/CameraFlowFragment.kt
@@ -0,0 +1,12 @@
+package com.bacon.camerax.android.presentation.ui.fragments.camera
+
+import com.bacon.camerax.android.R
+import com.bacon.camerax.android.presentation.base.BaseFlowFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CameraFlowFragment : BaseFlowFragment(
+    R.layout.flow_fragment_camera,
+    R.id.nav_host_fragment_container_camera
+) {
+}
